2154fa59df8d091ab5ac37723844c916e7136761
Louis Lee <pandawithcat@gmail.com>
2020-02-14 13:27:53 -0500

    some poc testing

---
+++ src/renderer/models/poc/dump/transactionstatus.js
@@ -0,0 +1,21 @@
+// source: model.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.api_proto.TransactionStatus');
+
+/**
+ * @enum {number}
+ */
+proto.api_proto.TransactionStatus = {
+  APPROVED: 0,
+  UNDER_REVIEW: 1,
+  CANCELLED: 2
+};
+
--- src/renderer/models/poc/fuck.js
+++ src/renderer/models/poc/fuck.js
@@ -3,7 +3,8 @@
 const protobuf = require('protobufjs');
 
 function traverseTypes(current, fn) {
-  if (current instanceof protobuf.Type) fn(current);
+  if (current instanceof protobuf.Type || current instanceof protobuf.Enum || current instanceof protobuf.OneOf)
+    fn(current);
   if (current.nestedArray)
     current.nestedArray.forEach(function(nested) {
       traverseTypes(nested, fn);
@@ -13,9 +14,9 @@ function traverseTypes(current, fn) {
 protobuf.load('./space_server.proto', function(err, root) {
   if (err) throw err;
 
-  traverseTypes(root, function(type) {
-    console.log(type.fullName);
-  });
-  // const message = root.lookupType('.api_proto.User');
-  // console.log(message.fields);
+  // traverseTypes(root, function(type) {
+  //   console.log(type.fullName);
+  // });
+  const message = root.lookupType('.api_proto.FullDefaultQuestion4_2');
+  console.log(message.fields);
 });
