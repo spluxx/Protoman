7d0e6603ce3ba4d316c076efe730dfb371bed84d
Louis Lee <pandawithcat@gmail.com>
2020-02-22 21:44:53 -0500

    used lodash instead of assert

--- src/renderer/models/poc/test/messageParserTest.ts
+++ src/renderer/models/poc/test/messageParserTest.ts
@@ -1,21 +1,21 @@
-import * as assert from 'assert';
 import { createMessageRecurse } from '../http/serializer';
 import { createMessageValue } from '../http/deserializer';
 import { userValue, userType, sampleCtx, test3UserExpectedJson } from './test3dummyValue';
 import { verifyJson } from './testUtils';
-
+import { isEqual } from 'lodash';
 export async function testMessageParser(): Promise<void> {
   console.log('Testing for Message Parser starting');
 
   console.log('message value to json');
   const jsonObject = createMessageRecurse(userValue);
-  assert.deepEqual(jsonObject, test3UserExpectedJson);
-
-  console.log('verify json');
-  await verifyJson('test3.User', './test3.proto', jsonObject);
+  isEqual(jsonObject, test3UserExpectedJson);
+  console.log('verifing json');
+  const errResult = await verifyJson('test3.User', './test3.proto', jsonObject);
+  isEqual(null, errResult);
 
   console.log('json to message value');
-  console.log(jsonObject);
-  const messageValue = createMessageValue(userType, jsonObject, sampleCtx);
-  console.log(messageValue);
+  const messageValue = createMessageValue(userType, test3UserExpectedJson, sampleCtx);
+  isEqual(messageValue, userValue);
+
+  console.log('all tests passed');
 }
--- src/renderer/models/poc/test/testUtils.ts
+++ src/renderer/models/poc/test/testUtils.ts
@@ -1,9 +1,8 @@
 import protobuf from 'protobufjs';
 
-export async function verifyJson(messageName: string, path: string, payload: any): Promise<void> {
+export async function verifyJson(messageName: string, path: string, payload: any): Promise<string | null> {
   const root = await protobuf.load(path);
   const userMessage = root.lookupType(messageName);
   const errMsg = userMessage.verify(payload);
-  console.log(errMsg);
-  if (errMsg) throw Error(errMsg);
+  return Promise.resolve(errMsg);
 }
